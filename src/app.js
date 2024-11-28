a578366c091fe01e8ff0603f7794fa5e63e8ba09
unknown <patilsign108@gmail.com>
2024-11-29 03:16:51 +0530

    explore express server

--- src/app.js
+++ src/app.js
@@ -2,17 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res) => {
-     res.send("Dashboard  From Express Js Server");
+// app.use("/test", (req, res) => {
+//      res.send("Test From Express Js Server...........");
+// });
+
+// app.use("/hello", (req, res) => {
+//      res.send("Hello From Express Js Server...........");
+// });
+
+// app.use("/", (req, res) => {
+//      res.send("Dashboard  From Express Js Server");
+// });
+
+
+app.get('/', (req, res) => {
+     res.send("first get call from express");
 });
 
-app.use("/test", (req, res) => {
-     res.send("Test From Express Js Server...........");
+const userData = { firstname: 'Santosh', lastname: 'patil' }
+app.get('/user', (req, res) => {
+     res.send(userData);
 });
 
-app.use("/hello", (req, res) => {
-     res.send("Hello From Express Js Server...........");
+app.post('/user', (req, res) => {
+     res.send("Data saved successfully");
 });
+
 
 app.listen(3000, () => {
      console.log("Server is listening an port 3000........");
